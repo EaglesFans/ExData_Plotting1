fc026b387306ae586847b861d4c34b6a644a268a
EaglesFans <94921357+EaglesFans@users.noreply.github.com>
2022-03-29 22:43:53 +0900

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -9,13 +9,13 @@ unzip("household_power_consumption.zip")
 ## Load the data set.
 df_1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "NA")
 
-## change the class of "Date" column from character to data.
+## change class of "Date" column from character to data.
 df_1$Date <- as.Date(df_1$Date, format = "%d/%m/%Y")
 
 ## Subset data from Feb 1 2007 to Feb 2 2007
 df_1_subset <- subset(df_1, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-## Change the class of Global_active_power from character to numeric
+## Change class of Global_active_power from character to numeric
 df_1_subset$Global_active_power <- as.numeric(df_1_subset$Global_active_power)
 
 ## Launch a PNG (graphic device) to create a PNG file with a width of 480 pixels and a height of 480 pixels
