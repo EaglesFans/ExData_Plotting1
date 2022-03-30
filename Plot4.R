b063af9e75f7958b4ced3780fce7f05da29a6b27
EaglesFans <94921357+EaglesFans@users.noreply.github.com>
2022-03-30 21:12:25 +0900

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -43,7 +43,7 @@ with(df_1_subset, plot(df_1_subset$DateTime, df_1_subset$Sub_metering_1, type = 
                        ylab = "Energy sub metering"))
 lines(df_1_subset$DateTime, df_1_subset$Sub_metering_2, type = "l", col = "red")
 lines(df_1_subset$DateTime, df_1_subset$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
 
 ## Make the 4th plot; Global Reactive Power over time
 with(df_1_subset, plot(df_1_subset$DateTime, df_1_subset$Global_reactive_power, type = "l", xlab = "datetime", 
